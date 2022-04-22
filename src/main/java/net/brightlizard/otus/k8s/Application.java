716a8fc82be7fd879ccba354e849331acb384fcf
Ovcharov Ilya <ovcharov.ilya@gmail.com>
2022-04-22 17:37:28 +0300

    Probes, ingress

--- src/main/java/net/brightlizard/otus/k8s/Application.java
+++ src/main/java/net/brightlizard/otus/k8s/Application.java
@@ -1,7 +1,6 @@
 package net.brightlizard.otus.k8s;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
@@ -9,7 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * net.brightlizard (c)
  */
 @SpringBootApplication
-@EnableAutoConfiguration
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
--- src/main/java/net/brightlizard/otus/k8s/rest/HealthController.java
+++ src/main/java/net/brightlizard/otus/k8s/rest/HealthController.java
@@ -1,5 +1,7 @@
 package net.brightlizard.otus.k8s.rest;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.util.MimeTypeUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,11 +13,32 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HealthController {
 
+    private boolean isHealthEnabled = true;
+
     @GetMapping(
         value = "/health",
         produces = MimeTypeUtils.APPLICATION_JSON_VALUE
     )
-    public String health(){
-        return "{\"status\": \"OK\"}";
+    public ResponseEntity<String> health(){
+        if(isHealthEnabled) return new ResponseEntity<>("{\"status\": \"OK\"}", HttpStatus.OK);
+        return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @GetMapping(
+            value = "/health/on",
+            produces = MimeTypeUtils.APPLICATION_JSON_VALUE
+    )
+    public String healthOn(){
+        isHealthEnabled = true;
+        return "{\"status\": \"DONE\"}";
+    }
+
+    @GetMapping(
+            value = "/health/off",
+            produces = MimeTypeUtils.APPLICATION_JSON_VALUE
+    )
+    public String healthOff(){
+        isHealthEnabled = false;
+        return "{\"status\": \"DONE\"}";
     }
 }
