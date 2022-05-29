21d9588a421d1cfd75b4e4f4052872c290b37dd3
Ovcharov Ilya <ovcharov.ilya@gmail.com>
2022-05-29 21:28:06 +0300

    latency

--- src/main/java/net/brightlizard/otus/k8s/rest/HealthController.java
+++ src/main/java/net/brightlizard/otus/k8s/rest/HealthController.java
@@ -1,13 +1,18 @@
 package net.brightlizard.otus.k8s.rest;
 
 import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.DistributionSummary;
 import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.MimeTypeUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Random;
 
 /**
  * @author Ovcharov Ilya (ovcharov.ilya@gmail.com)
@@ -18,9 +23,12 @@ public class HealthController {
 
     private boolean isHealthEnabled = true;
     private final Counter healthCounter;
+    private MeterRegistry meterRegistry;
 
     public HealthController(@Autowired MeterRegistry meterRegistry) {
-        this.healthCounter = meterRegistry.counter("health.counter");
+
+        this.healthCounter = meterRegistry.counter("health.counter", List.of(Tag.of("method", "GET"), Tag.of("uri", "/health")));
+        this.meterRegistry = meterRegistry;
     }
 
     @GetMapping(
@@ -28,8 +36,25 @@ public class HealthController {
         produces = MimeTypeUtils.APPLICATION_JSON_VALUE
     )
     public ResponseEntity<String> health(){
+        long start = System.currentTimeMillis();
         if(isHealthEnabled) {
+            int delay = new Random().nextInt(100);
+            try {
+                Thread.sleep(delay);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
             this.healthCounter.increment();
+            DistributionSummary latencySummary = DistributionSummary
+                    .builder("request.latency")
+                    .baseUnit("ms")
+                    .tags(List.of(Tag.of("method", "GET"), Tag.of("uri", "/health")))
+                    .publishPercentiles(0.5, 0.95, 0.99)
+                    .publishPercentileHistogram()
+                    .register(meterRegistry);
+            long latency = (System.currentTimeMillis() - start);
+            System.out.println("LATENCY = " + latency);
+            latencySummary.record(latency);
             return new ResponseEntity<>("{\"status\": \"OK\"}", HttpStatus.OK);
         }
         return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
