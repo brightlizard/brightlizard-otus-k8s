120dc39441b71cce3c9ad775eadbd79e0a7b1af1
Ovcharov Ilya <ovcharov.ilya@gmail.com>
2022-06-06 16:06:15 +0300

    +1

---
+++ src/main/java/net/brightlizard/otus/k8s/rest/EchoController.java
@@ -0,0 +1,46 @@
+package net.brightlizard.otus.k8s.rest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.MimeTypeUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Ovcharov Ilya (ovcharov.ilya@gmail.com)
+ * net.brightlizard (c)
+ */
+@RestController
+public class EchoController {
+
+    private boolean isHealthEnabled = true;
+
+    @GetMapping(
+        value = "/health",
+        produces = MimeTypeUtils.APPLICATION_JSON_VALUE
+    )
+    public ResponseEntity<String> health(){
+        if(isHealthEnabled) {
+            return new ResponseEntity<>("{\"status\": \"OK\"}", HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @GetMapping(
+        value = "/health/on",
+        produces = MimeTypeUtils.APPLICATION_JSON_VALUE
+    )
+    public String healthOn(){
+        isHealthEnabled = true;
+        return "{\"status\": \"DONE\"}";
+    }
+
+    @GetMapping(
+            value = "/health/off",
+            produces = MimeTypeUtils.APPLICATION_JSON_VALUE
+    )
+    public String healthOff(){
+        isHealthEnabled = false;
+        return "{\"status\": \"DONE\"}";
+    }
+}
